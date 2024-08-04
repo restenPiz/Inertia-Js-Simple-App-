1610b9c2e52adb811e823d5add2ea0d26c73926f
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-04 17:59:39 +0200

    Some updates in the application

--- inertia-app/resources/js/Pages/Welcome.jsx
+++ inertia-app/resources/js/Pages/Welcome.jsx
@@ -4,10 +4,11 @@ import DashboardIcon from '@rsuite/icons/legacy/Dashboard';
 import GroupIcon from '@rsuite/icons/legacy/Group';
 import MagicIcon from '@rsuite/icons/legacy/Magic';
 import GearCircleIcon from '@rsuite/icons/legacy/GearCircle';
+import '../Pages/styles/styles.css';
 
 function Welcome() {
     return (
-        <div style={{ width: 240 }}>
+        <div style={{ width: 240, height: '100vh', display: 'flex' }}>
             <Sidenav defaultOpenKeys={['3', '4']}>
                 <Sidenav.Body>
                     <Nav activeKey="1">
@@ -17,12 +18,6 @@ function Welcome() {
                         <Nav.Item eventKey="2" icon={<GroupIcon />}>
                             User Group
                         </Nav.Item>
-                        <Nav.Menu eventKey="3" title="Advanced" icon={<MagicIcon />}>
-                            <Nav.Item eventKey="3-1">Geo</Nav.Item>
-                            <Nav.Item eventKey="3-2">Devices</Nav.Item>
-                            <Nav.Item eventKey="3-3">Loyalty</Nav.Item>
-                            <Nav.Item eventKey="3-4">Visit Depth</Nav.Item>
-                        </Nav.Menu>
                         <Nav.Menu eventKey="4" title="Settings" icon={<GearCircleIcon />}>
                             <Nav.Item eventKey="4-1">Applications</Nav.Item>
                             <Nav.Item eventKey="4-2">Channels</Nav.Item>
