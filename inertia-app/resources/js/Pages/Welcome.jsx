f82af0e89c1ffdd7f67a5eacf3592813095279e0
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-04 19:40:11 +0200

    Some updates in the application

--- inertia-app/resources/js/Pages/Welcome.jsx
+++ inertia-app/resources/js/Pages/Welcome.jsx
@@ -2,35 +2,38 @@ import React from "react";
 import { Sidenav, Nav } from 'rsuite';
 import DashboardIcon from '@rsuite/icons/legacy/Dashboard';
 import GroupIcon from '@rsuite/icons/legacy/Group';
-import MagicIcon from '@rsuite/icons/legacy/Magic';
 import GearCircleIcon from '@rsuite/icons/legacy/GearCircle';
 import '../Pages/styles/styles.css';
 
 function Welcome() {
     return (
-        <div style={{ width: 240, height: '100vh', display: 'flex' }}>
-            <Sidenav defaultOpenKeys={['3', '4']}>
-                <Sidenav.Body>
-                    <Nav activeKey="1">
-                        <Nav.Item eventKey="1" icon={<DashboardIcon />}>
-                            Dashboard
-                        </Nav.Item>
-                        <Nav.Item eventKey="2" icon={<GroupIcon />}>
-                            User Group
-                        </Nav.Item>
-                        <Nav.Menu eventKey="4" title="Settings" icon={<GearCircleIcon />}>
-                            <Nav.Item eventKey="4-1">Applications</Nav.Item>
-                            <Nav.Item eventKey="4-2">Channels</Nav.Item>
-                            <Nav.Item eventKey="4-3">Versions</Nav.Item>
-                            <Nav.Menu eventKey="4-5" title="Custom Action">
-                                <Nav.Item eventKey="4-5-1">Action Name</Nav.Item>
-                                <Nav.Item eventKey="4-5-2">Action Params</Nav.Item>
-                            </Nav.Menu>
-                        </Nav.Menu>
-                    </Nav>
-                </Sidenav.Body>
-            </Sidenav>
-        </div>
+        <>
+            <div>
+                <div style={{ display: 'flex', height: '100vh' }}>
+                    <div style={{ width: 240 }}>
+                        <Sidenav defaultOpenKeys={['3', '4']}>
+                            <Sidenav.Body>
+                                <Nav activeKey="1">
+                                    <Nav.Item eventKey="1" icon={<DashboardIcon />}>
+                                        Dashboard
+                                    </Nav.Item>
+                                    <Nav.Item eventKey="2" icon={<GroupIcon />}>
+                                        All Truck
+                                    </Nav.Item>
+                                    <Nav.Menu eventKey="4" title="Settings" icon={<GearCircleIcon />}>
+                                        <Nav.Item eventKey="4-1">Add Users</Nav.Item>
+                                        <Nav.Item eventKey="4-2">Change Permissions</Nav.Item>
+                                    </Nav.Menu>
+                                </Nav>
+                            </Sidenav.Body>
+                        </Sidenav>
+                    </div>
+                    <div style={{ flexGrow: 1, padding: '20px' }}>
+                        <h1>Ola Mundo</h1>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 };
 
