3888acbbf9d2e0588d5e57bc1dc6496775490420
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-05 22:29:00 +0200

    Some updares in the application

--- inertia-app/resources/js/Pages/Welcome.jsx
+++ inertia-app/resources/js/Pages/Welcome.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from "react";
 import { Sidenav, Nav } from 'rsuite';
 import DashboardIcon from '@rsuite/icons/legacy/Dashboard';
@@ -8,26 +10,24 @@ import '../Pages/styles/styles.css';
 function Welcome() {
     return (
         <>
-            <div>
-                <div style={{ display: 'flex', height: '100vh' }}>
-                    <div style={{ width: 240 }}>
-                        <Sidenav defaultOpenKeys={['3', '4']}>
-                            <Sidenav.Body>
-                                <Nav activeKey="1">
-                                    <Nav.Item eventKey="1" icon={<DashboardIcon />}>
-                                        Dashboard
-                                    </Nav.Item>
-                                    <Nav.Item eventKey="2" icon={<GroupIcon />}>
-                                        All Truck
-                                    </Nav.Item>
-                                    <Nav.Menu eventKey="4" title="Settings" icon={<GearCircleIcon />}>
-                                        <Nav.Item eventKey="4-1">Add Users</Nav.Item>
-                                        <Nav.Item eventKey="4-2">Change Permissions</Nav.Item>
-                                    </Nav.Menu>
-                                </Nav>
-                            </Sidenav.Body>
-                        </Sidenav>
-                    </div>
+            <div style={{ display: 'flex', height: '100vh' }}>
+                <div style={{ width: 240 }}>
+                    <Sidenav defaultOpenKeys={['3', '4']}>
+                        <Sidenav.Body>
+                            <Nav activeKey="1">
+                                <Nav.Item eventKey="1" icon={<DashboardIcon />}>
+                                    Dashboard
+                                </Nav.Item>
+                                <Nav.Item eventKey="2" icon={<GroupIcon />}>
+                                    All Truck
+                                </Nav.Item>
+                                <Nav.Menu eventKey="4" title="Settings" icon={<GearCircleIcon />}>
+                                    <Nav.Item eventKey="4-1">Add Users</Nav.Item>
+                                    <Nav.Item eventKey="4-2">Change Permissions</Nav.Item>
+                                </Nav.Menu>
+                            </Nav>
+                        </Sidenav.Body>
+                    </Sidenav>
                 </div>
             </div>
         </>
