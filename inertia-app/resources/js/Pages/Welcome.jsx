cbccc4710eb52babd0b29ae353ae2e1dec5a381d
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-06 22:24:52 +0200

    Some updates in the application

--- inertia-app/resources/js/Pages/Welcome.jsx
+++ inertia-app/resources/js/Pages/Welcome.jsx
@@ -7,9 +7,9 @@ import Sidebar from "../Components/Sidebar";
 function Welcome() {
     return (
         //*Something will change here
-        <span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
-            Badge
-        </span>
+        <div>
+            {/*here will stay my first app using react*/}
+        </div>
     );
 };
 
