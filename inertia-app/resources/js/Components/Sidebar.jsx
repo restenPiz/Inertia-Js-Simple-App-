fd26c3fdcbc9dd90698ead25a9e3390f746cce10
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-06 23:16:47 +0200

    Some updates in the application

--- inertia-app/resources/js/Components/Sidebar.jsx
+++ inertia-app/resources/js/Components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import { Welcome } from "../Pages/Welcome";
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import Body from './Body';
 
 const user = {
     name: 'Tom Cook',
@@ -158,14 +158,7 @@ export default function Sidebar() {
                         </div>
                     </DisclosurePanel>
                 </Disclosure>
-                <header className="bg-white shadow">
-                    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                        <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-                    </div>
-                </header>
-                <main>
-                    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
-                </main>
+                <Body />
             </div>
         </>
     )
--- inertia-app/resources/js/Pages/Welcome.jsx
+++ inertia-app/resources/js/Pages/Welcome.jsx
@@ -1,17 +1,14 @@
 'use client';
 
 import React from "react";
-import Header from "../Components/Header";
 import Sidebar from "../Components/Sidebar";
 
 function Welcome() {
     return (
         //*Something will change here
-
         <>
             <Sidebar />
         </>
-
     );
 };
 
