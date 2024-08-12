35273a7adf0511a79be04aa7ac29f947e8eb0c70
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-12 21:04:27 +0200

    Some updates in the aplication

--- inertia-app/resources/js/Components/MainTable.jsx
+++ inertia-app/resources/js/Components/MainTable.jsx
@@ -1,46 +1,45 @@
-import { Card, Typography } from "@material-tailwind/react";
-import React from "react";
 
-export function MainTable() {
+import React from "react";
+import '../src/index.css';
+
+export default function MainTable() {
     return (
-        <Card className="h-full w-full overflow-scroll">
-            <table>
-                <thead>
-                    <tr>
-                        <th scope="col" rowspan="2">Name</th>
-                        <th scope="col" rowspan="2">ID</th>
-                        <th scope="col" colspan="2">Membership Dates</th>
-                        <th scope="col" rowspan="2">Balance</th>
-                    </tr>
-                    <tr>
-                        <th scope="col">Joined</th>
-                        <th scope="col">Canceled</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">Margaret Nguyen</th>
-                        <td>427311</td>
-                        <td><time datetime="2010-06-03">June 3, 2010</time></td>
-                        <td>n/a</td>
-                        <td>0.00</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Edvard Galinski</th>
-                        <td>533175</td>
-                        <td><time datetime="2011-01-13">January 13, 2011</time></td>
-                        <td><time datetime="2017-04-08">April 8, 2017</time></td>
-                        <td>37.00</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Hoshi Nakamura</th>
-                        <td>601942</td>
-                        <td><time datetime="2012-07-23">July 23, 2012</time></td>
-                        <td>n/a</td>
-                        <td>15.00</td>
-                    </tr>
-                </tbody>
-            </table>
-        </Card>
+        <table className="styled-table">
+            <thead>
+                <tr>
+                    <th scope="col" rowspan="2">Name</th>
+                    <th scope="col" rowspan="2">ID</th>
+                    <th scope="col" colspan="2">Membership Dates</th>
+                    <th scope="col" rowspan="2">Balance</th>
+                </tr>
+                <tr>
+                    <th scope="col">Joined</th>
+                    <th scope="col">Canceled</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Margaret Nguyen</th>
+                    <td>427311</td>
+                    <td><time datetime="2010-06-03">June 3, 2010</time></td>
+                    <td>n/a</td>
+                    <td>0.00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Edvard Galinski</th>
+                    <td>533175</td>
+                    <td><time datetime="2011-01-13">January 13, 2011</time></td>
+                    <td><time datetime="2017-04-08">April 8, 2017</time></td>
+                    <td>37.00</td>
+                </tr>
+                <tr>
+                    <th scope="row">Hoshi Nakamura</th>
+                    <td>601942</td>
+                    <td><time datetime="2012-07-23">July 23, 2012</time></td>
+                    <td>n/a</td>
+                    <td>15.00</td>
+                </tr>
+            </tbody>
+        </table>
     );
 }
