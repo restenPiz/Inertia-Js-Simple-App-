c99abe187671563f1158f8fe257b08034bfb1247
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-14 23:03:01 +0200

    Some updates in the application

--- inertia-app/resources/js/Components/MainTable.jsx
+++ inertia-app/resources/js/Components/MainTable.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import '../src/index.css';
 
 const columns = [
-    { Name: 'Truck Name', ID: 'Truck Weight', Member: 'Driver Name' },
+    { Name: 'Truck Name', ID: 'Truck Weight', Member: 'Driver Name', Park: 'Park Location' },
 ]
 const rows = [
-    { Name: 'Margaret Nguyen', ID: '427311', Member: 'June 3, 2010' },
-    { Name: 'Edvard Galinski', ID: '533175', Member: 'January 13, 2011' },
-    { Name: 'Hoshi Nakamura', ID: '533175', Member: 'January 13, 2011' },
-    { Name: 'Margaret Nguyen', ID: '427311', Member: 'June 3, 2010' },
-    { Name: 'Edvard Galinski', ID: '533175', Member: 'January 13, 2011' },
+    { Name: 'Margaret Nguyen', ID: '427311', Member: 'June 3, 2010', Park: 'Manica' },
+    { Name: 'Edvard Galinski', ID: '533175', Member: 'January 13, 2011', Park: 'Sofala' },
+    { Name: 'Hoshi Nakamura', ID: '533175', Member: 'January 13, 2011', Park: 'Zambezia' },
+    { Name: 'Margaret Nguyen', ID: '427311', Member: 'June 3, 2010', Park: 'Tete' },
+    { Name: 'Edvard Galinski', ID: '533175', Member: 'January 13, 2011', Park: 'Nampula' },
 ]
 
 export default function MainTable() {
