682cc9f42dc12a314d0aeab5bf3b7f91e3b3177a
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-14 22:23:08 +0200

    Some updates in the application

--- inertia-app/resources/js/Pages/Truck.jsx
+++ inertia-app/resources/js/Pages/Truck.jsx
@@ -23,11 +23,12 @@ export default function Truck() {
                             </label>
                             <div className="mt-2">
                                 <input
-                                    id="truck-name"
-                                    name="truck-name"
+                                    id="first-name"
+                                    name="first-name"
+                                    placeholder=" Ex: Scania MP300"
                                     type="text"
-                                    placeholder="Ex: Scania M399"
-                                    className="block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                    autoComplete="given-name"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -38,11 +39,12 @@ export default function Truck() {
                             </label>
                             <div className="mt-2">
                                 <input
-                                    id="truck-weight"
-                                    name="truck-weight"
+                                    id="first-name"
+                                    name="first-name"
+                                    placeholder=" Ex: 300kg"
                                     type="text"
-                                    placeholder="Ex: 300kg"
-                                    className="block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                    autoComplete="given-name"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
@@ -53,11 +55,12 @@ export default function Truck() {
                             </label>
                             <div className="mt-2">
                                 <input
-                                    id="truck-weight"
-                                    name="truck-weight"
+                                    id="first-name"
+                                    name="first-name"
+                                    placeholder=" Ex: Mauro Peniel"
                                     type="text"
-                                    placeholder="Ex: Mauro Peniel"
-                                    className="block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                    autoComplete="given-name"
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
                         </div>
