fde5f8c4893ca74a38e2771d30f296c33c74db36
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-14 14:19:22 +0200

    Some updates in the application

--- inertia-app/resources/js/Components/Sidebar.jsx
+++ inertia-app/resources/js/Components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import Body from './Body';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 const user = {
     name: 'Tom Cook',
