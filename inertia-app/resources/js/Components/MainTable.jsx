132a48b7c6f4c6fa0ee8b0f066aa36c14dcb914f
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-14 22:14:40 +0200

    Some updates in the application

--- inertia-app/resources/js/Components/MainTable.jsx
+++ inertia-app/resources/js/Components/MainTable.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import '../src/index.css';
 
 const columns = [
-    { Name: 'Nome', ID: 'ID', Member: 'Membership', Balance: 'Balance' },
+    { Name: 'Truck Name', ID: 'Truck Weight', Member: 'Driver Name' },
 ]
 const rows = [
-    { Name: 'Margaret Nguyen', ID: '427311', Member: 'June 3, 2010', Balance: 'n/a<' },
-    { Name: 'Edvard Galinski', ID: '533175', Member: 'January 13, 2011</time>', Balance: '37.00' },
-    { Name: 'Hoshi Nakamura', ID: '533175', Member: 'January 13, 2011</time>', Balance: '15.00' },
+    { Name: 'Margaret Nguyen', ID: '427311', Member: 'June 3, 2010' },
+    { Name: 'Edvard Galinski', ID: '533175', Member: 'January 13, 2011' },
+    { Name: 'Hoshi Nakamura', ID: '533175', Member: 'January 13, 2011' },
+    { Name: 'Margaret Nguyen', ID: '427311', Member: 'June 3, 2010' },
+    { Name: 'Edvard Galinski', ID: '533175', Member: 'January 13, 2011' },
 ]
 
 export default function MainTable() {
@@ -19,22 +21,19 @@ export default function MainTable() {
                     <tr>
                         <th scope="col" rowSpan="2">{column.Name}</th>
                         <th scope="col" rowSpan="2">{column.ID}</th>
-                        <th scope="col" colSpan="2"><time dateTime="2010-06-03">{column.Member}</time></th>
-                        <th scope="col" rowSpan="2">{column.Balance}</th>
+                        <th scope="col" colSpan="2">{column.Member}</th>
                     </tr>
                 ))}
             </thead>
             <tbody>
                 {rows.map(row => (
                     <tr>
-                        <th scope="row">{row.Name}</th>
-                        <td>{row.ID}</td>
-                        <td><time dateTime="2012-07-23">{row.Member}</time></td>
-                        <td>{row.Balance}</td>
-                        <td></td>
+                        <th scope="col">{row.Name}</th>
+                        <td scope="col">{row.ID}</td>
+                        <td scope="col">{row.Member}</td>
                     </tr>
                 ))}
             </tbody>
-        </table>
+        </table >
     );
 }
--- inertia-app/resources/js/Pages/Truck.jsx
+++ inertia-app/resources/js/Pages/Truck.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Sidebar from "../Components/Sidebar";
+import Table from "../Components/MainTable";
 
 export default function Truck() {
     return (
@@ -7,9 +8,76 @@ export default function Truck() {
             <Sidebar />
             <header className="bg-white shadow">
                 <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">Truck Managment</h1>
+                    <h1 className="text-3xl font-bold tracking-tight text-gray-900">Truck Management</h1>
                 </div>
             </header>
+
+            <div className="mx-auto max-w-7xl px-4 py-6 grid grid-cols-1 md:grid-cols-2 gap-6">
+
+                {/* Início do formulário */}
+                <form className="bg-white p-6 rounded-lg shadow">
+                    <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                        <div className="sm:col-span-4">
+                            <label htmlFor="truck-name" className="block text-sm font-medium leading-6 text-gray-900">
+                                Truck Name
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="truck-name"
+                                    name="truck-name"
+                                    type="text"
+                                    placeholder="Ex: Scania M399"
+                                    className="block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="sm:col-span-4">
+                            <label htmlFor="truck-weight" className="block text-sm font-medium leading-6 text-gray-900">
+                                Truck Weight
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="truck-weight"
+                                    name="truck-weight"
+                                    type="text"
+                                    placeholder="Ex: 300kg"
+                                    className="block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="sm:col-span-4">
+                            <label htmlFor="truck-weight" className="block text-sm font-medium leading-6 text-gray-900">
+                                Driver's Name
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="truck-weight"
+                                    name="truck-weight"
+                                    type="text"
+                                    placeholder="Ex: Mauro Peniel"
+                                    className="block w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="mt-6 flex items-center gap-x-6">
+                        <button
+                            type="submit"
+                            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            Save
+                        </button>
+                    </div>
+                </form>
+
+                {/* Tabela ao lado do formulário */}
+                <div className="bg-white p-6 rounded-lg shadow">
+                    <Table />
+                </div>
+            </div>
         </>
     );
 }
