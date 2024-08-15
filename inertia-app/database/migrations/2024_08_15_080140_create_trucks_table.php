f99d4af3357b8f941d8d53116ce3e8fb89a0ce53
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-15 11:08:36 +0200

    Some updates in the application

--- inertia-app/database/migrations/2024_08_15_080140_create_trucks_table.php
+++ inertia-app/database/migrations/2024_08_15_080140_create_trucks_table.php
@@ -12,10 +12,14 @@ return new class extends Migration {
             $table->string('Truck_name');
             $table->string('Truck_weight');
             $table->string('Truck_code');
-            $table->string('Park_location');
             $table->string('Driver_name');
             $table->string('Rest_days');
             $table->string('Total_value');
+
+            //*Inicio da chave estrangeira
+            $table->unsignedBigInteger('id');
+            $table->foreign('Park_location')->references('id')->on('parks');
+
             $table->timestamps();
         });
     }
