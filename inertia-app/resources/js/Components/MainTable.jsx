8c89be493c8eca3723ee8c61935ed936693ea2ce
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-15 14:12:05 +0200

    Some updates in the application

--- inertia-app/resources/js/Components/MainTable.jsx
+++ inertia-app/resources/js/Components/MainTable.jsx
@@ -1,39 +1,41 @@
-
 import React from "react";
-import '../src/index.css';
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@shadcn/ui";
 
 const columns = [
     { Name: 'Truck Name', ID: 'Truck Weight', Member: 'Driver Name', Park: 'Park Location' },
-]
+];
+
 const rows = [
     { Name: 'Margaret Nguyen', ID: '427311', Member: 'June 3, 2010', Park: 'Manica' },
     { Name: 'Edvard Galinski', ID: '533175', Member: 'January 13, 2011', Park: 'Sofala' },
     { Name: 'Hoshi Nakamura', ID: '533175', Member: 'January 13, 2011', Park: 'Zambezia' },
     { Name: 'Margaret Nguyen', ID: '427311', Member: 'June 3, 2010', Park: 'Tete' },
     { Name: 'Edvard Galinski', ID: '533175', Member: 'January 13, 2011', Park: 'Nampula' },
-]
+];
 
 export default function MainTable() {
     return (
-        <table className="styled-table">
-            <thead>
-                {columns.map(column => (
-                    <tr>
-                        <th scope="col" rowSpan="2">{column.Name}</th>
-                        <th scope="col" rowSpan="2">{column.ID}</th>
-                        <th scope="col" colSpan="2">{column.Member}</th>
-                    </tr>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    {columns.map((column, index) => (
+                        <React.Fragment key={index}>
+                            <TableCell rowSpan={2}>{column.Name}</TableCell>
+                            <TableCell rowSpan={2}>{column.ID}</TableCell>
+                            <TableCell colSpan={2}>{column.Member}</TableCell>
+                        </React.Fragment>
+                    ))}
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {rows.map((row, index) => (
+                    <TableRow key={index}>
+                        <TableCell>{row.Name}</TableCell>
+                        <TableCell>{row.ID}</TableCell>
+                        <TableCell>{row.Member}</TableCell>
+                    </TableRow>
                 ))}
-            </thead>
-            <tbody>
-                {rows.map(row => (
-                    <tr>
-                        <th scope="col">{row.Name}</th>
-                        <td scope="col">{row.ID}</td>
-                        <td scope="col">{row.Member}</td>
-                    </tr>
-                ))}
-            </tbody>x
-        </table>
+            </TableBody>
+        </Table>
     );
 }
