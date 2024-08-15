4aa8ea3207cebe720a4ed46ec75d92bbaa5686f8
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-15 10:04:04 +0200

    Some updates in the application

--- inertia-app/routes/web.php
+++ inertia-app/routes/web.php
@@ -2,20 +2,24 @@
 
 use App\Http\Controllers\mainController;
 use App\Http\Controllers\Park\parkController;
+use App\Http\Controllers\Settings\settingsController;
 use App\Http\Controllers\Truck\truckController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-//*Inicio da rota mae
+//*Inicio da rota principa;
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('Welcome');
 
 Route::get('/index', [mainController::class, 'index']);
 
-//*Inicio das rotas de camioes
+//?Inicio das rotas de camioes
 Route::get('/truck', [truckController::class, 'index']);
 Route::get('/storeTruck', [truckController::class, 'store']);
 
-//*Inicio das rotas do parque
+//?Inicio das rotas do parque
 Route::get('/park', [parkController::class, 'index']);
+
+//?Inicio da rota da parte de definicoes
+Route::get('/settings', [settingsController::class, 'index']);
