e5bf495df92e9ae52cc32d053fa42163ff2b857b
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-18 19:27:09 +0200

    Some updates in the controllerPark

--- inertia-app/app/Http/Controllers/Park/parkController.php
+++ inertia-app/app/Http/Controllers/Park/parkController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Park;
 
 use App\Http\Controllers\Controller;
+use App\Models\Park;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -11,5 +12,38 @@ class parkController extends Controller
     public function index()
     {
         return Inertia::render('Park');
+    }
+    public function savePark()
+    {
+        $table = new Park();
+
+        $table->Park_name = Request::input('Park_name');
+        $table->Park_location = Request::input('Park_location');
+        $table->Park_capacity = Request::input('Park_capacity');
+
+        $table->save();
+
+        return back();
+    }
+    public function delete($id)
+    {
+        $park = Park::findOrFail($id);
+
+        $park->delete();
+
+        return back();
+    }
+    public function updatePark($id)
+    {
+        $table = Park::findOrFail($id);
+
+        $table->Park_name = Request::input('Park_name');
+        $table->Park_location = Request::input('Park_location');
+        $table->Park_capacity = Request::input('Park_capacity');
+        $table->Id = Request::input('Id');
+
+        $table->save();
+
+        return back();
     }
 }
--- inertia-app/resources/js/Pages/Truck.jsx
+++ inertia-app/resources/js/Pages/Truck.jsx
@@ -65,7 +65,7 @@ export default function Truck() {
                                     name="first-name"
                                     placeholder="Ex: 300kg"
                                     type="text" value={values.Truck_weight}
-                                    autoComplete="given-name" onChange={handleChange}
+                                    autoComplete="given-name2" onChange={handleChange}
                                     className="block w-full p-6 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
                             </div>
