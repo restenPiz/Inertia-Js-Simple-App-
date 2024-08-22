7a5b0cc4feb49c918981e5b2ae3dc7713240294f
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-22 18:44:09 +0200

    Some updates in the application

--- inertia-app/resources/js/Pages/parkTable.jsx
+++ inertia-app/resources/js/Pages/parkTable.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import '../src/index.css';
 
 const columns = [
-    { Name: 'Park Name', ID: 'Park Location' },
+    { Name: 'Park Name', ID: 'Park Location', Total: 'Total_Capacity' },
 ]
 const rows = [
-    { Name: 'Margaret Nguyen', ID: '427311', },
-    { Name: 'Edvard Galinski', ID: '533175', },
-    { Name: 'Hoshi Nakamura', ID: '533175', },
-    { Name: 'Margaret Nguyen', ID: '427311', },
-    { Name: 'Edvard Galinski', ID: '533175', },
+    { Name: 'Margaret Nguyen', ID: '427311', Total: '60' },
+    { Name: 'Edvard Galinski', ID: '533175', Total: '70' },
+    { Name: 'Hoshi Nakamura', ID: '533175', Total: '80' },
+    { Name: 'Margaret Nguyen', ID: '427311', Total: '90' },
+    { Name: 'Edvard Galinski', ID: '533175', Total: '100' },
 ]
 
 export default function parkTable() {
@@ -21,6 +21,7 @@ export default function parkTable() {
                     <tr>
                         <th scope="col" rowSpan="2">{column.Name}</th>
                         <th scope="col" rowSpan="2">{column.ID}</th>
+                        <th scope="col" rowSpan="2">{column.Total}</th>
                     </tr>
                 ))}
             </thead>
@@ -29,6 +30,7 @@ export default function parkTable() {
                     <tr>
                         <th scope="col">{row.Name}</th>
                         <td scope="col">{row.ID}</td>
+                        <td scope="col">{row.Total}</td>
                     </tr>
                 ))}
             </tbody>
