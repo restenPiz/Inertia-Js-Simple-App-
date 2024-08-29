c7b883344e3ca08ab5043b7e34fbd5bac66deb2a
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-29 10:07:44 +0200

    Some updates in the application

--- inertia-app/app/Http/Controllers/Park/parkController.php
+++ inertia-app/app/Http/Controllers/Park/parkController.php
@@ -21,26 +21,13 @@ class parkController extends Controller
             'Park_capacity' => ['required', 'max:50'],
         ]));
 
-        return to_route('index');
+        return response()->json(['message' => 'Parque adicionado com sucesso!']);
     }
     public function delete($id)
     {
         $park = Park::findOrFail($id);
 
         $park->delete();
-
-        return back();
-    }
-    public function updatePark($id)
-    {
-        $table = Park::findOrFail($id);
-
-        $table->Park_name = Request::input('Park_name');
-        $table->Park_location = Request::input('Park_location');
-        $table->Park_capacity = Request::input('Park_capacity');
-        $table->Id = Request::input('Id');
-
-        $table->save();
 
         return back();
     }
