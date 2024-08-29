aa8a33b73765d3c9d1ed28a6ab5cb7668b9090f4
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-29 20:29:53 +0200

    Some updates in the application

--- inertia-app/resources/js/Pages/parkTable.jsx
+++ inertia-app/resources/js/Pages/parkTable.jsx
@@ -26,12 +26,12 @@ export default function ParkTable({ parks }) {
                         <td>{park.Park_location}</td>
                         <td>{park.Park_capacity}</td>
                         <td>
-                            <button
+                            <button onSubmit={handleEdit}
                                 className="px-4 py-2 mr-2 text-sm font-medium text-white bg-blue-500 rounded hover:bg-blue-600"
                             >
                                 Edit
                             </button>
-                            <button
+                            <button onSubmit={handleDelete}
                                 className="px-4 py-2 text-sm font-medium text-white bg-red-500 rounded hover:bg-red-600"
                             >
                                 Delete
