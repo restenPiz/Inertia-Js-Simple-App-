791e572064dabce6b5aa28076c2d3b3cd8ac1fa3
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-08-30 21:13:35 +0200

    Some updates in the application

--- inertia-app/resources/js/Components/Sidebar.jsx
+++ inertia-app/resources/js/Components/Sidebar.jsx
@@ -57,16 +57,6 @@ export default function Sidebar() {
                         </div>
                         <div className="hidden md:block">
                             <div className="ml-4 flex items-center md:ml-6">
-                                {/* <button
-                                    type="button"
-                                    className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                                >
-                                    <span className="absolute -inset-1.5" />
-                                    <span className="sr-only">View notifications</span>
-                                    <BellIcon aria-hidden="true" className="h-6 w-6" />
-                                </button> */}
-
-                                {/* Profile dropdown */}
                                 <Menu as="div" className="relative ml-3">
                                     <div>
                                         <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
